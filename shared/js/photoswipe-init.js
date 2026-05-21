18ea5dc537cec6f48dfcf2dd547a947518026997
Sophia Dassen <dassensophia@gmail.com>
2026-05-22 00:53:40 +0200

    Add PhotoSwipe video plugin for mp4 viewing

--- shared/js/photoswipe-init.js
+++ shared/js/photoswipe-init.js
@@ -1,6 +1,9 @@
 import PhotoSwipeLightbox from
     'https://cdn.jsdelivr.net/npm/photoswipe@5/dist/photoswipe-lightbox.esm.min.js';
 
+import PhotoSwipeVideoPlugin from 
+    './vendor/photoswipe-video-plugin.esm.js';
+
 const lightbox = new PhotoSwipeLightbox({
 
     gallery: 'a[data-pswp-width]',
@@ -9,6 +12,10 @@ const lightbox = new PhotoSwipeLightbox({
     pswpModule: () =>
     import('https://cdn.jsdelivr.net/npm/photoswipe@5/dist/photoswipe.esm.min.js')
 
+});
+
+const videoPlugin = new PhotoSwipeVideoPlugin(lightbox, {
+    // options if needed
 });
 
 lightbox.init();
