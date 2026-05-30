67d42cb2d9e4e7e9483ef3cae5a6d1d537bf06ff
Sophia Dassen <dassensophia@gmail.com>
2026-05-30 19:27:38 +0200

    Modify full-screen image expand on desktop and optimize image resolutions

--- shared/js/photoswipe-init.js
+++ shared/js/photoswipe-init.js
@@ -4,10 +4,23 @@ import PhotoSwipeLightbox from
 import PhotoSwipeVideoPlugin from 
     './vendor/photoswipe-video-plugin.esm.js';
 
+function isDesktop() {
+    return window.matchMedia('(min-width: 900px)').matches;
+}
+
 const lightbox = new PhotoSwipeLightbox({
 
     gallery: 'a[data-pswp-width]',
     children: '',
+
+    initialZoomLevel: (zoomLevelObject) => {
+
+        if (isDesktop()) {
+            return zoomLevelObject.fit * 0.85;
+        }
+
+        return zoomLevelObject.fit;
+    },
 
     pswpModule: () =>
     import('https://cdn.jsdelivr.net/npm/photoswipe@5/dist/photoswipe.esm.min.js')
@@ -19,3 +32,4 @@ const videoPlugin = new PhotoSwipeVideoPlugin(lightbox, {
 });
 
 lightbox.init();
+
